e3b10fabce702457c031fc1f4c32b4019a08cc5b
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2025-12-18 09:30:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort in specific order/sort-in-specific-order.cpp
+++ Difficulty: Medium/Sort in specific order/sort-in-specific-order.cpp
@@ -2,26 +2,15 @@ class Solution {
   public:
     void sortIt(vector<int>& arr) {
         // code here
-        vector<int>odd;
-        vector<int>even;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]%2==0){
-                even.push_back(arr[i]);
-            }else{
-                odd.push_back(arr[i]);
+        sort(arr.begin(),arr.end(),[](int a,int b){
+            if(a%2==0 && b%2==0){
+                return a<b;
             }
-        }
-        sort(odd.begin(),odd.end());
-        sort(even.begin(),even.end());
-        
-        int k=0;
-        for(int i=odd.size()-1;i>=0;i--){
-            arr[k++]=odd[i];
-        }
-        
-        for(int i=0;i<even.size();i++){
-            arr[k++]=even[i];
-        }
+            if(a%2!=0 && b%2!=0){
+                return a>b;
+            }
+            return (a%2!=0);
+        });
         
         
     }
