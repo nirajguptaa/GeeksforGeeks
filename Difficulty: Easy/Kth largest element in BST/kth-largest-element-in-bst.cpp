ed7fad90cf270b94759107d2cccb9e7513c0cd9f
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-21 13:19:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Kth largest element in BST/kth-largest-element-in-bst.cpp
+++ Difficulty: Easy/Kth largest element in BST/kth-largest-element-in-bst.cpp
@@ -14,18 +14,20 @@ struct Node {
 // return the Kth largest element in the given BST rooted at 'root'
 class Solution {
   public:
-    int rev_in(Node *root,int &k){
-        if(root==NULL)return -1;
-        int right=rev_in(root->right,k);
+    int reverseInorder(Node*root,int &k){
+        if(root==NULL){
+            return -1;
+        }
+        int right=reverseInorder(root->right,k);
         if(right!=-1){
             return right;
         }
         k--;
         if(k==0)return root->data;
-        return rev_in(root->left,k);
+        return reverseInorder(root->left,k);
     }
     int kthLargest(Node *root, int k) {
         // Your code here
-        return rev_in(root,k);
+        return reverseInorder(root,k);
     }
 };
