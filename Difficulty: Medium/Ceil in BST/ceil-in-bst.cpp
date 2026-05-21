db2cc23db38a2f0c4ffb73aaf819056bc9c8cf5b
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-21 12:05:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
+++ Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
@@ -1,18 +1,36 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
 class Solution {
   public:
+    void findCeil(Node *root,int k,int &CeilVal){
+       if(root==NULL)return ;
+        if(root->data>=k ){
+            CeilVal=min(CeilVal,root->data);
+            findCeil(root->left,k,CeilVal);
+        }else{
+            
+            findCeil(root->right,k,CeilVal);
+        }
+        
+        
+    }
     int findCeil(Node* root, int x) {
         // code here
-        int ceil_val=-1;
-        while(root!=NULL){
-            if(root->data==x){
-                return x;
-            }else if(root->data>x){
-                ceil_val=root->data;
-                root=root->left;
-            }else{
-                root=root->right;
-            }
-        }
-        return ceil_val;
+        int CeilVal=INT_MAX;
+        findCeil(root,x,CeilVal);
+        return CeilVal==INT_MAX?-1:CeilVal;
     }
 };
