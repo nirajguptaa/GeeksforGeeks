85f53ff9956622acd8beb86bc35ac26b4a15774e
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-22 12:17:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -2,36 +2,37 @@ class Solution {
   public:
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
-        vector<vector<int>>adj(V);
+        vector<int>adj[V];
+        
+       
+        vector<int>indegree(V);
         for(auto &it:edges){
             int u=it[0];
             int v=it[1];
             adj[u].push_back(v);
+            indegree[v]++;
         }
-        vector<int>inDegree(V);
-        for(auto &it:adj){
-            for(auto node:it){
-                inDegree[node]++;
-            }
-        }
+        vector<int>topoSortArr;
         queue<int>q;
         for(int i=0;i<V;i++){
-            if(inDegree[i]==0){
-                q.push(i);
-            }
+            if(indegree[i]==0)q.push(i);
+            
         }
-        vector<int>ans;
         while(!q.empty()){
-            int ele=q.front();
+            int node=q.front();
             q.pop();
-            ans.push_back(ele);
-            for(auto node:adj[ele]){
-                inDegree[node]--;
-                if(inDegree[node]==0){
-                    q.push(node);
+            topoSortArr.push_back(node);
+            for(int neigh:adj[node]){
+                
+                indegree[neigh]--;
+                if(indegree[neigh]==0){
+                    q.push(neigh);
                 }
+                
             }
         }
-        return ans;
+        return topoSortArr; 
+        
+        
     }
 };
