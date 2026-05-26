01813f8dd593f4a1640d1241858f669abea2081d
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-26 07:13:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,28 +1,24 @@
 class Solution {
   public:
-    int f(int ind,int W, vector<int> &val, vector<int> &wt,vector<vector<int>>&dp){
-        if(ind==0){
-            if(wt[ind]<=W){
-                return val[ind];
-                
-            }
+    int f(int idx,int W,vector<int> &val, vector<int> &wt,vector<vector<int>>&dp){
+        if(idx==wt.size()){
             return 0;
         }
-        if(dp[ind][W]!=-1){
-            return dp[ind][W];
+        
+        if(dp[idx][W]!=-1){
+            return dp[idx][W];
         }
-        int notpick=f(ind-1,W,val,wt,dp);
-        int pick=0;
-        if(wt[ind]<=W){
-            pick=val[ind]+f(ind-1,W-wt[ind],val,wt,dp);
+        int notake=f(idx+1,W,val,wt,dp);
+        int take=0;
+        if((W-wt[idx])>=0){
+            take=val[idx]+f(idx+1,W-wt[idx],val,wt,dp);
         }
-        return dp[ind][W]=max(pick,notpick);
+        return dp[idx][W]=max(take,notake);
     }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
         int n=val.size();
         vector<vector<int>>dp(n,vector<int>(W+1,-1));
-        return f(n-1,W,val,wt,dp);
-        
+        return f(0,W,val,wt,dp);
     }
 };
