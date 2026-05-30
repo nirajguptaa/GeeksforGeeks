ad2eb6ace0227b2221c39a0ab82d2a24d339d0bb
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-30 08:54:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.cpp
+++ Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.cpp
@@ -1,11 +1,12 @@
 // User function Template for C++
 class Disjoint{
+    private:
     vector<int>parent,size;
     public:
     Disjoint(int n){
         parent.resize(n+1);
         size.resize(n+1,1);
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<=n;i++){
             parent[i]=i;
         }
     }
@@ -13,46 +14,40 @@ class Disjoint{
         if(node==parent[node]){
             return node;
         }
-        return parent[node]=findUPar(parent[node]);
+        return parent[node]=findUPar(parent[node]); 
     }
-    void unionBySize(int u,int v){
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-        if(ulp_u==ulp_v)return ;
-        if(size[ulp_u]<size[ulp_v]){
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
+    void unionSize(int u,int v){
+        int ultP_U=findUPar(u);
+        int ultP_V=findUPar(v);
+        if(ultP_U==ultP_V)return;
+        if(size[ultP_U]<size[ultP_V]){
+            parent[ultP_U]=ultP_V;
+            size[ultP_V]+=size[ultP_U];
         }else{
-             parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
+            parent[ultP_V]=ultP_U;
+            size[ultP_U]+=size[ultP_V];
         }
     }
-    
 };
-bool cmp(vector<int>&a,vector<int>&b){
-    return a[2]<b[2];
-}
 class Solution {
   public:
-    int kruskalsMST(int V, vector<vector<int>> &edges) {//u v wt
-        
-        // sort(edges.begin(),edges.end(),
-        //     [](auto &a,auto &b){return a[2]<b[2];} 
-        // );
-        
+    static bool cmp(vector<int>&a,vector<int>&b){
+        return a[2]<b[2];
+    }
+    int kruskalsMST(int V, vector<vector<int>> &edges) {
+        // code here
         sort(edges.begin(),edges.end(),cmp);
-        Disjoint ds(V);
-        int mstWt=0;
-        for(auto edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-            int w=edge[2];
-            if(ds.findUPar(u)!=ds.findUPar(v)){
-                mstWt+=w;
-                ds.unionBySize(u,v);
+        Disjoint d(V);
+        int ans=0;
+        for(auto &e:edges){
+            int u=e[0];
+            int v=e[1];
+            int wt=e[2];
+            if(d.findUPar(u)!=d.findUPar(v)){
+                ans+=wt;
+                d.unionSize(u,v);
             }
         }
-        return mstWt;
+        return ans;
     }
-    
 };
