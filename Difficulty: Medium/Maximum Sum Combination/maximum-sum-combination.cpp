6c466f229fd7cc74b9ed8e02cdda90500a9553ed
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-08-24 14:46:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
+++ Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
@@ -4,27 +4,26 @@ class Solution {
         // code here
         sort(a.begin(),a.end(),greater<int>());
         sort(b.begin(),b.end(),greater<int>());
+        vector<int>ans;
+        set<pair<int,int>>vis;
         priority_queue<pair<int,pair<int,int>>>pq;
         pq.push({a[0]+b[0],{0,0}});
-        set<pair<int,int>>vis;
-        vector<int>ans;
         while(!pq.empty() && k--){
-            int sum=pq.top().first;
-            int i=pq.top().second.first;
-            int j=pq.top().second.second;
-            ans.push_back(sum);
+            int val=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;
             pq.pop();
-            vis.insert({i,j});
-            if(i+1<a.size() && vis.find({i+1,j})==vis.end()){
-                pq.push({a[i+1]+b[j],{i+1,j}});
-                vis.insert({i+1,j});
+            ans.push_back(val);
+            vis.insert({row,col});
+            while(row+1<a.size() && vis.find({row+1,col})==vis.end()){
+                pq.push({a[row+1]+b[col],{row+1,col}});
+                vis.insert({row+1,col});
             }
-            if(j+1<b.size() && vis.find({i,j+1})==vis.end()){
-                pq.push({a[i]+b[j+1],{i,j+1}});
-                vis.insert({i,j+1});
+            while(col+1<b.size() && vis.find({row,col+1})==vis.end()){
+                pq.push({a[row]+b[col+1],{row,col+1}});
+                vis.insert({row,col+1});
             }
         }
         return ans;
-        
     }
 };
